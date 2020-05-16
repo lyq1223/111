8c804703b7fa9c893f4d3085c02797c632e6848e
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-05-16 23:27:09 +0800

    mongodb

--- index.js
+++ index.js
@@ -1,39 +1,77 @@
-class Car{
-    // es6构建
-    constructor(number,name){
-        this.name=name;
-        this.number=number;
-    }
+// const _users = [ 
+//   {
+//     "address": {
+//       "city": "Los Angeles",
+//       "state": 'California',
+//       "pincode": "123"
+//     },
+//     "tags": [
+//       "music",
+//       "blogs",
+//       "cricket"
+//     ],
+//     "name": "Tom Benzamin"
+//   },
+//   {
+//     "address": {
+//       "city": "赣州",
+//       "state": '江西',
+//       "pincode": "331000"
+//     },
+//     "tags": [
+//       "coding",
+//       "blogs"
+//     ],
+//     "name": "明天"
+//   },
+//   {
+//     "address": {
+//       "city": "九江",
+//       "state": '江西',
+//       "pincode": "331000"
+//     },
+//     "tags": [
+//       "coding",
+//       "swim"
+//     ],
+//     "name": "襄阳"
+//   },
+//     {
+//     "address": {
+//       "city": "赣州",
+//       "state": '江西',
+//       "pincode": "331000"
+//     },
+//     "tags": [
+//       "coding",
+//       "games"
+//     ],
+//     "name": "颜如玉"
+//   }
+// ]
+//真数据已经有了，就不需要假数据了
+export default {
+  fetchUsers(cb) {
+    // setTimeout(() => {
+    //   cb(_users);
+    // }, 1000)
+    //获取数据库中的数据，从node-user的app.js
+    fetch('/api/users') //转发到/users:3000下
+    .then(data => data.json()) //没有用axios,降低难度，fetch后将二进制流json化    
+    .then(data =>{
+      console.log(data, '----api跨域');
+      cb(data)
+    })
+    .catch(err =>{
+      cb(err)
+    })
+  },
+  fetchUsersByTag(tag, cb) {  //cb:callback回调函数
+    fetch(`/api/users/tag/${tag}`)
+    .then(data => data.json())
+    .then(data => {
+      console.log(data);
+      cb(data)
+    })
+  }
 }
-// 父类到子类的继承
-class Kuaiche extends Car{
-    constructor(number,name){
-        // 执行父类的构造函数
-        super(number,name);
-        this.price=1;
-    }
-}
-class Zhuanche extends Car{
-    constructor(number,name){
-        super(number,name);
-        this.price=2;
-    }
-}
-// 统筹每个对象间的关系
-class Trip{
-    constructor(car,length){
-        this.car=car;
-        this.length=length;
-    }
-    start(){
-        console.log(`行程开始了，名称${this.car.name},车牌号:${this.car.number}`);
-    }
-    end(){
-        console.log(`行程结束，价钱是:${this.car.price*this.length}元`);
-    }
-}
-// 实例子类
-let car=new Kuaiche('京A88888','宝马X6');
-let trip=new Trip(car,10);
-trip.start();
-trip.end();
