a1e919b2583b3e8866ab7156cdf5d7422a8de8a8
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-06-18 20:07:12 +0800

    leetcode14

--- 1.js
+++ 1.js
@@ -1,17 +1,16 @@
-var myAtoi = function(str) {
-   
-    // parseInt(string, radix)   将一个字符串 string 转换为 radix 进制的整数， radix 为介于2-36之间的数。
-    const result = parseInt(str, 10)
-    //如果是字符串开头，parseInt会返回NaN 所以要先判断是否是NaN,是就返回0
-    if(isNaN(result)) {
-        return 0;
-    } else if(result < Math.pow(-2,31)) {  //Math.pow可以求几次方:-2^31
-        return Math.pow(-2,31);
-    } else if(result > Math.pow(2, 31)-1) {
-        return Math.pow(2, 31)-1;
+var longestCommonPrefix = function(strs) {
+    if(!strs || strs.length === 0) return ''
+    let res = '';
+    let index = 0;
+    for(const c of strs[0]) {
+        for(let i = 1; i < strs.length; i++ ) {
+            if(index >= strs[i].length || strs[i][index] !== c ) {
+                return res
+            }            
+        }
+        res += c;
+        index++
     }
-    return result;
+    return res;
 };
-console.log(myAtoi("4193 with words"));
-console.log(myAtoi("-91283472332"));
-console.log(myAtoi("words and 987"));
+console.log(longestCommonPrefix(["flower","flow","flight"]));
