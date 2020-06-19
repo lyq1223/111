a887afa7e103e5ff294462d38c7fb703898f8db9
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-06-19 19:00:35 +0800

    leetcode22

--- 1.js
+++ 1.js
@@ -1,36 +1,19 @@
-function listNode(value){
-    this.value = value;
-    this.next = null;
-}
-const a1 = new listNode(1);
-const a2 = new listNode(2);
-const a3 = new listNode(4);
-const b1 = new listNode(1);
-const b2 = new listNode(3);
-const b3 = new listNode(4);
-a1.next = a2;
-a2.next = a3;
-b1.next = b2;
-b2.next = b3;
-
-//递归
-var mergeTwoLists = function(l1, l2) {
-    // console.log(l1);
-    let res = new listNode();
-    if(l1 === null) {
-        return l2;
-    }
-    if(l2 === null) {
-         return l1;
-    }
-    if(l1.value >= l2.value) {
-        res = l2;
-        res.next = mergeTwoLists(l1, l2.next);
-    } else {
-        res = l1;
-        res.next = mergeTwoLists(l1.next, l2);
-    }
+// 添加右括号时，左括号数量必须大于右括号数量，并且右括号 < n
+var generateParenthesis = function(n) {
+    let res = [];
+    const parent = function(cur, left, right) {
+        if(cur.length === 2*n) {
+            res.push(cur);
+            return;
+        }
+        if(left < n) {
+            parent(cur + '(',left+1, right);
+        }
+        if(right < left){
+            parent(cur+')',left, right+1);
+        }
+    };
+    parent('',0,0);
     return res;
 };
-console.log(mergeTwoLists(a1,b1));
-// console.log(a1);
+console.log(generateParenthesis(3))
