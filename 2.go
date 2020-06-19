0338be77234c67e60551ae4c85a86c74f11bd52a
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-06-19 18:57:05 +0800

    leetcode21go

--- 2.go
+++ 2.go
@@ -1,30 +1,47 @@
-func longestCommonPrefix(strs []string) string {
-    if len(strs) == 0 {
-        return ""
-    }
-    prefix := strs[0]
-    count := len(strs)
-    for i := 1; i < count; i++ {
-        prefix = lcp(prefix, strs[i])
-        if len(prefix) == 0 {
-            break
-        }
-    }
-    return prefix
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
 }
 
-func lcp(str1, str2 string) string {
-    length := min(len(str1), len(str2))
-    index := 0
-    for index < length && str1[index] == str2[index] {
-        index++
-    }
-    return str1[:index]
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	if l1.Val >= l2.Val {
+		l2.Next = mergeTwoLists(l1, l2.Next)
+		return l2
+	} else {
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	}
 }
 
-func min(x, y int) int {
-    if x < y {
-        return x
-    }
-    return y
+func main() {
+	// ???
+	a1 := new(ListNode)
+	a1.Val = 1
+	a2 := new(ListNode)
+	a2.Val = 2
+	a3 := new(ListNode)
+	a3.Val = 3
+	b1 := new(ListNode)
+	b1.Val = 1
+	b2 := new(ListNode)
+	b2.Val = 3
+	b3 := new(ListNode)
+	b3.Val = 4
+	a1.Next = *(&a2)
+	a2.Next = *(&a3)
+	b1.Next = *(&b2)
+	b2.Next = *(&b3)
+	fmt.Println(mergeTwoLists(a1, b1))
+	// mergeTwoLists(a1, b2)
+
 }
