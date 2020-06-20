9ce0e2abc5ebe19f047cb928d0e26f1c62bd4ad0
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-06-20 16:56:22 +0800

    leetcode42

--- 1.js
+++ 1.js
@@ -1,49 +1,26 @@
-var search = function(nums, target) {
-     // 关键点，因为数组是升序的，所以通过mid判断哪边是有序的很重要
-     if(!nums || nums.length ===0) return -1;
-     let start = 0;
-     let end = nums.length - 1;
-     let mid;
-     while(start <= end) {
-         // 通过不断变动的mid来判断
-         mid = Math.ceil(nums.length / 2);
-         if (nums[start] === target) return start;
-         if (nums[end] === target) return end;
-         if (nums[mid] === target) return mid;
-         if (nums[start] < nums[mid]) {
-             // 左边是有序的
-             if (target > nums[start] && target < nums[mid]) {
-                 end = mid - 1;
-             } else {
-                 start = mid + 1;
-             }
-         } else {
-             // 右边是有序的
-             if (target >nums[mid] && target < nums[end]) {
-                 start = mid + 1;
-             } else {
-                 end = mid - 1;
-             }
-         }
-     }
-     return -1
-
-    //  超出时间限制？？
-    // 用了这个
-    var low = 0;
-    var high = nums.length - 1;
-    while(low < high){
-        var mid = (low+high)>>1; //除二了 神奇
-        if( target < nums[0] && target > nums[mid]){
-            low = mid+1;
+var trap = function(height) {
+    let countsum = 0;     
+    height.forEach((item, index) => {
+        let left = 0, right = 0;
+        for(var i = 0; i <= index; i++) {  //0->0,1->0,1,2->0,1,2,3->...->0,1,...,8
+            // console.log(i);
+            left = Math.max(height[i], left)
+            // console.log(left,'======')
         }
-        else if(nums[0] <= nums[mid] && (target > nums[mid] || target < nums[0])) {
-            low = mid+1;
+        for (var i = index; i < height.length; i++) { //0->1->2->...->8
+            // console.log(i);
+            right = Math.max(height[i], right)
+            // console.log(right,'======')
         }
-        else{
-            high = mid;
-        }
-    }
-    return low == high && nums[low] == target ? low : -1;
+        // console.log(left,right);// 每轮的加起来
+        countsum +=Math.min(left, right) -item
+    });
+    
+    // return left;
+    return countsum
 };
-console.log(search([4, 5, 6, 7, 0, 1, 2], 0));
+// console.log(trap([0,1,0,2,1,0,1,3,2,1,2,1]));
+// console.log(trap([1,0,1]));
+console.log(trap([2, 0, 2, 3, 2, 2, 2, 1, 2]));
+
+
